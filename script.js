fd0cd86c7b35b89bf363210274537a1bf894382e
Renata Veras Venturim <129551549+RenataVerasVenturim@users.noreply.github.com>
2023-06-11 14:57:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Objetivo: Capturar 2 números, e somá-los*/
+/* Definição das Variáveis */
+var js_primeironumero;
+var js_segundonumero;
+var js_soma;
+
+function somarnumeros() {
+/* Entrada de Dados */
+
+js_primeironumero = parseInt(document.getElementById('j_primeironumero').value);
+js_segundonumero = parseInt(document.getElementById('j_segundonumero').value);
+/* Processamento de Dados */
+js_soma= js_primeironumero + js_segundonumero;
+/* Saída de Dados */
+window.alert('Resultado da soma: ' + js_soma);
+}
